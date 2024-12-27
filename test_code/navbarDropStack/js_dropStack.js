58783196ce783654dc78c39530710c9838ae6c58
chrisshuo123 <chr1z100@gmail.com>
2024-12-28 02:22:35 +0700

    arranging folder for html, css, and JS

--- test_code/navbarDropStack/js_dropStack.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navbarLinks = document.getElementById('navbar-links');
-const navbar = document.querySelector('.navbar');
-
-menuToggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
