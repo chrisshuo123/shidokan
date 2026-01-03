02749850e94a2d7d75e7b930fbefcb46b8b5adee
chrisshuo123 <chr1z100@gmail.com>
2026-01-03 22:05:00 +0700

    Add route to the detail page (when the client or an amdin on index instructor main CRUD page clicks on an instructor's detail)

--- admin/app/controllers/Instruktur.php
+++ admin/app/controllers/Instruktur.php
@@ -3,9 +3,18 @@
 class Instruktur extends Controller {
     public function index() {
         $data['judul'] = "Instruktur";
+        $data['instrukturList'] = $this->model('Instruktur_model')->getInstrukturList();
 
         $this->view('templates/header', $data);
         $this->view('instruktur/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function detail() {
+        $data['judul'] = "Detail Instruktur";
+
+        $this->view('templates/header', $data);
+        $this->view('instruktur/detail', $data);
         $this->view('templates/footer');
     }
 }
