c4a847cdf68ae03a0fc74001967fa68a6e4cfa3c
chrisshuo123 <chr1z100@gmail.com>
2026-01-03 22:05:41 +0700

    In the main instructor list CRUD, we add the 'blackbelt' and 'detail' column. On this same commit, we add the DB Row List that contains the detailed instructor list

--- admin/app/views/instruktur/index.php
+++ admin/app/views/instruktur/index.php
@@ -10,11 +10,12 @@
                     <th scope="col">Nama</th>
                     <th scope="col">Branch</th>
                     <th scope="col">Status</th>
-                    <th scope="col">CRUD Editor</th>
+                    <th scope="col">Blackbelt Level</th>
+                    <th scope="col">Detail</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <!-- <tr>
                     <th scope="row">1</th>
                     <td>Tikori Mahesa</td>
                     <td>Flores</td>
@@ -24,7 +25,29 @@
                         <a href="#"><span class="badge text-bg-warning">Ubah</span></a>
                         <a href="#"><span class="badge text-bg-danger">Delete</span></a>
                     </td>
-                </tr>
+                </tr> -->
+                <?php $counter = 1; ?>
+                <?php if(!empty($data['instrukturList'])) : ?>
+                    <?php foreach($data['instrukturList'] as $listInstruktur): ?>
+                        <tr>
+                            <th scope="row"><?= htmlspecialchars($counter); ?></th>
+                            <td><?= htmlspecialchars($listInstruktur['namaDepan']); ?>&nbsp;<?= htmlspecialchars($listInstruktur['namaBelakang']); ?></td>
+                            <td><?= htmlspecialchars($listInstruktur['branch']); ?></td>
+                            <td><?= htmlspecialchars($listInstruktur['status']); ?></td>
+                            <td><?= htmlspecialchars($listInstruktur['levelDan']) ?></td>
+                            <td>
+                                <a href="#"><span class="badge text-bg-primary">Detail</span></a>
+                            </td>
+                        </tr>
+                        <?php $counter++ ?>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <tr>
+                        <td colspan="6" class="text-center">
+                            <div class="alert alert-warning">Tidak ada data instruktur</div>
+                        </td>
+                    </tr>
+                <?php endif; ?>
             </tbody>
         </table>
     </div>
