be65c69fe0d5cbf75a30ab9c6a0db192c633d807
chrisshuo123 <chr1z100@gmail.com>
2026-01-03 22:08:23 +0700

    Add new views/dojo/index.php page as the main Dojo CRUD List

---
+++ admin/app/views/dojo/index.php
@@ -0,0 +1,47 @@
+    <div class="container">
+        <h1>Panel Dojo</h1>
+        <button type="button" class="btn btn-primary tombolTambahDataUser" style="margin: 1.5% 0 1.5% 0;" data-bs-toggle="modal" data-bs-target="#formModalUser">
+            Tambah Dojo
+        </button>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama Dojo</th>
+                    <th scope="col">Alamat Dojo</th>
+                    <th scope="col">Foto</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- <tr>
+                    <th scope="row">1</th>
+                    <td>Tikori Mahesa</td>
+                    <td>Flores</td>
+                    <td>Aktif</td>
+                    <td>
+                        <a href="#"><span class="badge text-bg-primary">Detail</span></a>
+                        <a href="#"><span class="badge text-bg-warning">Ubah</span></a>
+                        <a href="#"><span class="badge text-bg-danger">Delete</span></a>
+                    </td>
+                </tr> -->
+                <?php $counter = 1; ?>
+                <?php if(!empty($data['dojoList'])) : ?>
+                    <?php foreach($data['dojoList'] as $listInstruktur): ?>
+                        <tr>
+                            <th scope="row"><?= htmlspecialchars($counter); ?></th>
+                            <td><?= htmlspecialchars($listInstruktur['namaDojo']); ?></td>
+                            <td><?= htmlspecialchars($listInstruktur['alamatDojo']); ?></td>
+                            <td><?= htmlspecialchars($listInstruktur['foto']); ?></td>
+                        </tr>
+                        <?php $counter++ ?>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <tr>
+                        <td colspan="6" class="text-center">
+                            <div class="alert alert-warning">Tidak ada data Dojo</div>
+                        </td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
