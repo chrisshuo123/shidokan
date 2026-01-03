d1d40aa0545f2a07f6080f0efa3f864cbdaf21cd
chrisshuo123 <chr1z100@gmail.com>
2026-01-03 22:04:20 +0700

    refine the DB_NAME to 'shidokan_db' to connect with the correct DB Scheme name via localhost phpmyadmin

--- admin/app/config/config.php
+++ admin/app/config/config.php
@@ -6,7 +6,7 @@ define('BASEURL', 'http://localhost/shidokan-web/admin/public');
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3307');
-define('DB_NAME', 'shidokan');
+define('DB_NAME', 'shidokan_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
