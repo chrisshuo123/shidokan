fd18253fb883c1f26b981fd270e79f6237532066
chrisshuo123 <chr1z100@gmail.com>
2026-01-03 22:06:06 +0700

    Add the route to core/Database.php

--- admin/app/init.php
+++ admin/app/init.php
@@ -2,5 +2,6 @@
 
 require_once __DIR__ . '/core/App.php';
 require_once __DIR__ . '/core/Controller.php';
+require_once __DIR__ . '/core/Database.php';
 
 require_once __DIR__ . '/config/config.php';
