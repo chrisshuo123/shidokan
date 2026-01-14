4a30814add626e7da1664746c31bac37d0e76dbf
chrisshuo123 <chr1z100@gmail.com>
2026-01-15 06:22:41 +0700

    In index method, connecting the FK Instructor by calling 3 methods from Instruktur_model (branch, status, and levelDan table), and in method detail (call the getInstrukturById method from Instruktur_model). In this method, we add 2 new method which are tambah(), and getUbahInstruktur()

--- admin/app/controllers/Instruktur.php
+++ admin/app/controllers/Instruktur.php
@@ -5,15 +5,38 @@ class Instruktur extends Controller {
         $data['judul'] = "Instruktur";
         $data['instrukturList'] = $this->model('Instruktur_model')->getInstrukturList();
 
+        // Data untuk dropdown di modal tambah
+        $data['branchList'] = $this->model('Instruktur_model')->getBranchList();
+        $data['statusList'] = $this->model('Instruktur_model')->getStatusList();
+        $data['levelDanList'] = $this->model('Instruktur_model')->getLevelDanList();
+
         $this->view('templates/header', $data);
         $this->view('instruktur/index', $data);
         $this->view('templates/footer');
     }
 
+    public function tambah() {
+        $data['judul'] = "Tambah Instruktur";
+        
+        // Jika form disubmit
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Debug data yang dikirim form
+            // echo '<pre>';
+            // var_dump($_POST);
+            // echo '</pre>';
+            // die();
+        
+            // Setelah debugging berhasil, baru panggil ini:
+            $this->model('Instruktur_model')->tambahInstruktur($_POST);
+        }
+        header('Location: '. BASEURL . '/instruktur');
+        exit;
+    }
+
     public function detail($idInstruktur) {
         $data['judul'] = "Detail Instruktur";
-        $data['instrukturDetail'] = $this->model('Instruktur_model')->getInstrukturDetail($idInstruktur);
-
+        $data['instrukturDetail'] = $this->model('Instruktur_model')->getInstrukturById($idInstruktur);
+        
         // Debug: Check what's being returned
         // echo '<pre>';
         // var_dump($data['instrukturDetail']);
@@ -23,5 +46,21 @@ class Instruktur extends Controller {
         $this->view('templates/header', $data);
         $this->view('instruktur/detail', $data);
         $this->view('templates/footer');
+    }
+
+    public function getUbahInstruktur() {
+        echo json_encode($this->model('Instruktur_model')->getInstrukturDetail($_POST['idInstruktur']));
+    }
+
+    // public function ubahInstruktur() {
+    //     $this->model('Instruktur_model')->
+    // }
+
+    public function hapus($idInstruktur) {
+        $data['judul'] = "Hapus Instruktur";
+        $data['instrukturHapus'] = $this->model('Instruktur_model')->deleteInstruktur($idInstruktur);
+
+        header('Location: '. BASEURL . '/instruktur');
+        exit;
     }
 }
